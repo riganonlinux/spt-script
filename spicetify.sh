7d7e44856ff1496b5ac3e894990929bd5886f986
riganonlinux <76118779+riganonlinux@users.noreply.github.com>
2020-12-17 01:42:28 +0530

    Add files via upload

---
+++ spicetify.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+<<COMMENT
+Author : Rigan Burnwal
+Date : 16th Dec, 2020
+Email : riganonlinux@gmail.com
+This script is meant to simplify the process of setting up themes on Spotify using spicetify, it automates the download, install and set-up process, plus provides an easy to use alias: set-spt-theme for getting the work done in an easy way, feel free to improve it or mail me suggestions.
+COMMENT
+
+clear
+
+#set repo and install spotify deb
+echo Installing the Spotify deb
+cd $HOME
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+read -p "Press return to continue..."
+clear
+
+#change spotify access permissions
+echo Adding access to Spotify
+sudo chmod a+wr /usr/share/spotify
+sudo chmod a+wr /usr/share/spotify/Apps -R
+read -p "Press return to continue..."
+clear
+
+#install spicetify
+echo Installing and setting up Spicetify
+curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
+./spicetify-cli/spicetify config prefs_path $HOME/.config/spotify/prefs
+./spicetify-cli/spicetify -n -q backup
+./spicetify-cli/spicetify -n apply
+read -p "Press return to continue..."
+clear
+
+#add shortcut alias to spicetify
+echo Adding alias to spicetify
+echo -e alias spotify-themes=\"ls $HOME/.config/spicetify/Themes\" >> $HOME/.bash_aliases
+echo -e alias spicetify=\"$HOME/spicetify-cli/spicetify\"
+echo -e "alias set-spt-theme='spotify-themes && read -p \"Enter the chosen theme: \" THEME && spicetify config current_theme $THEME && spicetify apply'" >> $HOME/.bash_aliases
+read -p "Done, press return to continue..."
+clear
+
+#install themes
+echo Installing git, do not worry if already installed
+sudo apt install git
+git clone https://github.com/morpheusthewhite/spicetify-themes.git
+echo Creating Themes folder and copying themes
+mkdir -vp $HOME/.config/spicetify/Themes
+cd spicetify-themes
+cp -r * $HOME/.config/spicetify/Themes
+cd ..
+rm -rf spicetify-themes
+read -p "Press return to continue..."
+clear
+
+#list all the commands
+echo
+echo Listing all important commands :
+echo spotify-themes : lists all the Spotify themes
+echo set-spt-theme : lists all the themes, lets you choose and sets it up
+echo
+source ~/.bashrc
+echo Enjoy your spicetify!
+
+
